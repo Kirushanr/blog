49a07b38a715dd2f70dec5b664c1b056cc30bf88
Kirushan Rasendran <3458013+Kirushanr@users.noreply.github.com>
2025-12-18 09:14:40 +1100

    Favicons added

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1765924010',resources: [
+    cacheName: 'chirpy-1766009570',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
